a0f0f69aa2be066c1cb8bfd5006e49723e4e701a
Andrei Drexler <makro@rq3.com>
2024-01-17 23:05:22 +0100

    Use stb_image to load png, jpg and tga files (#295)
    This addresses a long-standing TODO in Image_LoadImage,
    and also works around the content issues in the new Enyo
    add-on (skyboxes using jpeg files with tga extension).

--- Quake/image.h
+++ Quake/image.h
@@ -25,9 +25,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 //image.h -- image reading / writing
 
-//be sure to free the hunk after using these loading functions
-byte *Image_LoadTGA (FILE *f, int *width, int *height);
-byte *Image_LoadPCX (FILE *f, int *width, int *height);
+//be sure to free the hunk after using this loading function
 byte *Image_LoadImage (const char *name, int *width, int *height);
 
 qboolean Image_WriteTGA (const char *name, byte *data, int width, int height, int bpp, qboolean upsidedown);
