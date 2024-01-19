61f21d782e342f1e8d37bb43e12fd2ea773bb0d0
Andrei Drexler <makro@rq3.com>
2024-01-19 21:24:11 +0100

    Add STBI_NO_LINEAR macro

--- Quake/image.c
+++ Quake/image.c
@@ -33,6 +33,7 @@ static byte *Image_LoadPCX (FILE *f, int *width, int *height);
 #define STBI_NO_HDR
 #define STBI_NO_PIC
 #define STBI_NO_PNM
+#define STBI_NO_LINEAR
 #include "stb_image.h"
 
 #define STB_IMAGE_WRITE_IMPLEMENTATION
