945c277025fe017ea76079a360e04d0956779d8f
xDShot <miha-xdshot@yandex.ru>
2024-02-08 11:52:57 +0300

    IN_ResetCurrentController I forgot to commit

--- Quake/input.h
+++ Quake/input.h
@@ -53,6 +53,8 @@ qboolean IN_IsCalibratingGyro (void);
 
 qboolean IN_HasLED (void);
 
+void IN_ResetCurrentController (void);
+
 qboolean IN_HasGamepad (void);
 const char *IN_GetGamepadName (void);
 void IN_UseNextGamepad (int dir, qboolean allow_disable);
